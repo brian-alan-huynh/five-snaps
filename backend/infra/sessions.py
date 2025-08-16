2993a205374ca73bbd7d5a027d49f1bfd23a9b1f
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-08-15 21:16:34 -0700

    Add Confluent Kafka for Redis sessions

--- backend/infra/sessions.py
+++ backend/infra/sessions.py
@@ -1,36 +1,67 @@
 import os
+import json
 import uuid
 from datetime import datetime
 
-import redis
 from dotenv import load_dotenv
+
+from .messaging import kafka_producer
+from ..config import REDIS_CLIENT
 
 load_dotenv()
 env = os.getenv
 
 class Redis:
-    def __init__(self):
-        self.redis_client = redis.Redis(
-            host=env("REDIS_HOST"),
-            port=int(env("REDIS_PORT")),
-            decode_responses=True,
-            username="default",
-            password=env("REDIS_USER_PASS"),
-        )
+    @staticmethod
+    def add_new_session(user_id: int, oauth_access_token: str) -> str | bool:
+        try: 
+            session_id = str(uuid.uuid4())
+            session_key = f"session:{session_id}"
+            
+            message = {
+                "operation": "add_new_session",
+                "session_key": session_key,
+                "user_id": user_id,
+                "oauth_access_token": oauth_access_token,
+                "created_at": datetime.now(),
+            }
 
-    def add_new_session(self, user_id: int) -> str:
-        session_id = str(uuid.uuid4())
-        session_key = f"session:{session_id}"
+            kafka_producer.produce(
+                topic="redis.add_new_session",
+                value=json.dumps(message).encode("utf-8"),
+            )
+
+            kafka_producer.flush()
+
+            return session_key
+
+        except Exception:
+            return False
+
+    @staticmethod
+    def get_session(session_key: str) -> dict | bool:
+        try:
+            return REDIS_CLIENT.hgetall(session_key)
         
-        self.redis_client.hset(session_key, mapping={
-            "user_id": user_id,
-            "created_at": datetime.now(),
-        })
+        except Exception:
+            return False
 
-        return session_key
+    @staticmethod
+    def delete_session(session_key: str) -> bool:
+        try:
+            message = {
+                "operation": "delete_session",
+                "session_key": session_key,
+            }
 
-    def get_session(self, session_key: str) -> dict:
-        return self.redis_client.hgetall(session_key)
+            kafka_producer.produce(
+                topic="redis.delete_session",
+                value=json.dumps(message).encode("utf-8"),
+            )
 
-    def delete_session(self, session_key: str) -> None:
-        self.redis_client.delete(session_key)
+            kafka_producer.flush()
+
+            return True
+
+        except Exception:
+            return False
