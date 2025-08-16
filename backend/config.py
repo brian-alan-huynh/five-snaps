87123e0809c55742cac28b1dd8cbf7c3e0ed844a
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-08-15 22:05:29 -0700

    Add Redis configs

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,22 @@ import os
 from typing import Final
 
 import boto3
+import redis
 from dotenv import load_dotenv
 
 load_dotenv()
 env = os.getenv
 
+# Redis
+REDIS_CLIENT: Final[redis.Redis] = redis.Redis(
+    host=env("REDIS_HOST"),
+    port=int(env("REDIS_PORT")),
+    decode_responses=True,
+    username="default",
+    password=env("REDIS_USER_PASS"),
+)
 
+# S3
 S3_CLIENT: Final[boto3.client] = boto3.client(
     "s3",
     aws_access_key_id=env("AWS_S3_ACCESS_KEY_ID"),
@@ -16,3 +26,5 @@ S3_CLIENT: Final[boto3.client] = boto3.client(
 )
 
 BUCKET_NAME: Final[str] = env("AWS_S3_BUCKET_NAME")
+
+# write kafka config for facebook post uploads
