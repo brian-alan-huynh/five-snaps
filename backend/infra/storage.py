e19c5eaf5bac60cf1420c75009a7ab55ec70435d
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-08-15 20:59:10 -0700

    Add view all images feature

--- backend/infra/storage.py
+++ backend/infra/storage.py
@@ -7,7 +7,7 @@ from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 from fastapi import UploadFile
 
-from ..config import BUCKET_NAME
+from ..config import S3_CLIENT, BUCKET_NAME
 from .messaging import kafka_producer
 
 load_dotenv()
@@ -22,7 +22,7 @@ class S3:
         return f"{user_id}/snap/{timestamp}_{unique_id}{file_extension}"
 
     @classmethod
-    async def upload_snap(cls, user_id: int, file: UploadFile) -> str | bool:
+    async def upload_snap(cls, user_id: int, file: UploadFile) -> bool:
         try:
             file_extension = os.path.splitext(file.filename)[1].lower()
             
@@ -46,8 +46,29 @@ class S3:
             
             kafka_producer.flush()
             
-            return f"https://{BUCKET_NAME}.s3.{env('AWS_REGION')}.amazonaws.com/{s3_key}"
+            return True
         
+        except ClientError:
+            return False
+        
+    @staticmethod
+    def read_snap(user_id: int) -> list[dict[str, str | datetime]] | bool:
+        try:
+            response = S3_CLIENT.list_objects_v2(Bucket=BUCKET_NAME, Prefix=f"{user_id}/snap/")
+            
+            if "Contents" not in response:
+                return False
+            
+            return [
+                {
+                    "img_url": f"https://{BUCKET_NAME}.s3.{env("AWS_S3_REGION")}.amazonaws.com/{obj["Key"]}",
+                    "created_at": obj["LastModified"],
+                    "file_size": obj["Size"],
+                    "s3_key": obj["Key"],
+                }
+                for obj in response["Contents"]
+            ]
+            
         except ClientError:
             return False
 
