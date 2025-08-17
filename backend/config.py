06eb6150dec90015b09ad421f24781910d87f285
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-08-16 17:29:26 -0700

    Remove unecessary comments and whitespace

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,6 @@ from pymongo import MongoClient
 load_dotenv()
 env = os.getenv
 
-# Redis
 REDIS_CLIENT = redis.Redis(
     host=env("REDIS_HOST"),
     port=int(env("REDIS_PORT")),
@@ -17,19 +16,14 @@ REDIS_CLIENT = redis.Redis(
     password=env("REDIS_USER_PASS"),
 )
 
-# S3
 S3_CLIENT = boto3.client(
     "s3",
     aws_access_key_id=env("AWS_S3_ACCESS_KEY_ID"),
     aws_secret_access_key=env("AWS_S3_SECRET_ACCESS_KEY"),
     region_name=env("AWS_S3_REGION")
 )
-
 BUCKET_NAME = env("AWS_S3_BUCKET_NAME")
 
-# MongoDB
 MONGO_CLIENT = MongoClient(env("MONGO_CONNECTION_STRING"))
 MONGO_DB = MONGO_CLIENT[env("MONGO_DB_NAME")]
 MONGO_COLLECTION = MONGO_DB.image_tags
-
-# write kafka config for facebook post uploads
