dece2c3a3df93ea05b4b276403d8b51490c1a70b
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-08-20 23:43:22 -0700

    Wrap logic in class and try/except block + Add format for log msgs

--- backend/logging_config.py
+++ backend/logging_config.py
@@ -9,27 +9,35 @@ from dotenv import load_dotenv
 load_dotenv()
 env = os.getenv
 
-log_queue = Queue(-1)
-queue_handler = logging.handlers.QueueHandler(log_queue)
+class Logging:
+    def __init__(self):
+        log_queue = Queue(-1)
+        self.queue_handler = logging.handlers.QueueHandler(log_queue)
 
-loki_handler = logging_loki.LokiHandler(
-    url=env("GRAFANA_LOKI_URL"),
-    tags={ "application": "five-snaps" },
-    auth=(env("GRAFANA_LOKI_USERNAME"), env("GRAFANA_LOKI_PASSWORD")),
-    version="1",
-)
+        loki_handler = logging_loki.LokiHandler(
+            url=env("GRAFANA_LOKI_URL"),
+            tags={ "application": "five-snaps" },
+            auth=(env("GRAFANA_LOKI_USERNAME"), env("GRAFANA_LOKI_PASSWORD")),
+            version="1",
+        )
 
-listener = logging.handlers.QueueListener(log_queue, loki_handler)
-listener.start()
+        self.listener = logging.handlers.QueueListener(log_queue, loki_handler)
+        self.listener.start()
+        
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=logging.ERROR,
+        )
 
-logger = logging.getLogger("five_snaps_error_logger")
-logger.setLevel(logging.ERROR)
-logger.addHandler(queue_handler)
+        self.logger = logging.getLogger("five_snaps_error_logger")
+        self.logger.setLevel(logging.ERROR)
+        self.logger.addHandler(self.queue_handler)
 
-def log_error(error_message: str) -> None:
-    logger.error(error_message)
+    def log_error(self, error_message: str) -> None:
+        self.logger.error(error_message)
 
-def terminate_logging() -> None:
-    listener.stop()
-    logging.shutdown()
-    logger.removeHandler(queue_handler)
+    def terminate_logging(self) -> None:
+        self.listener.stop()
+        self.listener.join()
+        logging.shutdown()
+        self.logger.removeHandler(self.queue_handler)
